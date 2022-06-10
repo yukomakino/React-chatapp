87836b1318f9ba5c7a50b575868e048ce59d95cb
yuko makino <yucom2.99@gmail.com>
2022-06-11 01:43:52 +0900

    React Router(v6)の導入

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from "react";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Room from "./pages/Room";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App = () => {
     return (
-        <>
-            <Login />
-            <SignUp />
-            <Room />
-        </>
+        <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Room />} />
+                    <Route path='/login' element={<Login/>} />
+                    <Route path='/signup' element={<SignUp/>} />
+                </Routes>
+        </BrowserRouter>
     );
 };
 
