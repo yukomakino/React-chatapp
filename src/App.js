0394d9803ef1679d5111f8bb3698cd2c6595e686
yuko makino <yucom2.99@gmail.com>
2022-06-11 00:41:43 +0900

    サインアップフォームの作成

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Login from "./pages/Login";
+import SignUp from "./pages/SignJp";
 
 const App = () => {
     return (
         <>
             <Login />
+            <SignUp />
         </>
     );
 };
