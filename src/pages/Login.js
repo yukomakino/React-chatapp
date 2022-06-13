b8fd2287ac067101b229f04b1974c49c1f3b22d9
yuko makino <yucom2.99@gmail.com>
2022-06-13 11:50:20 +0900

    ログイン機能の実装

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,25 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import firebase from '../config/firebase';
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventdefault();
+        firebase
+            .auth()
+            .signInWithEmailAndPassword(email, password)
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+
     return (
         <>
             <h1>Login</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor='email'>E-mail</label>
                     <input 
                         type='email' 
+                        value={email}
                         id='email' 
-                        name='email' placeholder='Email' 
+                        name='email' 
+                        placeholder='Email' 
+                        onChange={e => setEmail(e.target.value)}
                     />
                 </div>
                 <div>
                     <label htmlFor='password'>Password</label>
                     <input 
                         type='password'
+                        value={password}
                         id='password'
                         name='password'
                         placeholder='password'
+                        onChange={e => setPassword(e.target.value)}
                     />
                 </div>
                 <button type='submit'>Login</button>
